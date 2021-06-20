0cae39e605894483bca18cecf5c5b25ab4fe9d38
Tateishi Tadatoshi <ishio39@gmail.com>
2021-06-20 18:55:27 +0900

    change SECRET_KEY

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gwrih7pxdbxtew@q_ia1e3wcg#c*=9_5@yo7fo*pnxoefqgbg='
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
